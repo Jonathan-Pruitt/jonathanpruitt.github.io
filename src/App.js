3d89ae6b5f6f18f7e04fbec4375175add737d9f1
Jonathan-Pruitt <130386433+Jonathan-Pruitt@users.noreply.github.com>
2024-06-21 08:41:15 -0500

    Update Portfolio

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Knowledge from "./components/Knowledge";
+import Bio from "./components/Bio";
+import Bored from "./components/Bored";
+import Contact from "./components/Contact";
+import Navigator from "./components/Navigator";
+
+function App() {    
+
+    
+
+    return (
+        <div className="container">
+            <Navigator />
+            <div className="row justify-content-center">
+                <div className="col-md-10 container">
+                    <Header />
+                    <Projects />
+                    <Knowledge />
+                    <Bio /> 
+                    <Bored />            
+                    <Contact />
+                </div>
+            </div>
+        </div>
+    )
+}//end component
+
+export default App;
